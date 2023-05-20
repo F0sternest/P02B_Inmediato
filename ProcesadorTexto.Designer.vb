4e55937f426b50a908ce38347002c343c1030bd6
F0sternest <kevin.rod2026@gmail.com>
2023-05-20 10:19:07 -0500

    Añadiendo los cambios inmediatos de el color de fuente y mi funcionalidad del boton limpiar

--- ProcesadorTexto.Designer.vb
+++ ProcesadorTexto.Designer.vb
@@ -22,23 +22,24 @@ Partial Class formProcesadorTexto
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.txtResultado = New System.Windows.Forms.TextBox()
         Me.GroupBox1 = New System.Windows.Forms.GroupBox()
         Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.btnVerde = New System.Windows.Forms.Button()
+        Me.btnAzul = New System.Windows.Forms.Button()
+        Me.btnRojo = New System.Windows.Forms.Button()
         Me.GroupBox3 = New System.Windows.Forms.GroupBox()
-        Me.Button1 = New System.Windows.Forms.Button()
-        Me.Button2 = New System.Windows.Forms.Button()
-        Me.Button3 = New System.Windows.Forms.Button()
-        Me.Button4 = New System.Windows.Forms.Button()
+        Me.btnLimpiar = New System.Windows.Forms.Button()
         Me.GroupBox2.SuspendLayout()
         Me.SuspendLayout()
         '
-        'TextBox1
+        'txtResultado
         '
-        Me.TextBox1.Location = New System.Drawing.Point(96, 52)
-        Me.TextBox1.Name = "TextBox1"
-        Me.TextBox1.Size = New System.Drawing.Size(599, 20)
-        Me.TextBox1.TabIndex = 0
+        Me.txtResultado.ForeColor = System.Drawing.SystemColors.WindowText
+        Me.txtResultado.Location = New System.Drawing.Point(96, 52)
+        Me.txtResultado.Name = "txtResultado"
+        Me.txtResultado.Size = New System.Drawing.Size(599, 20)
+        Me.txtResultado.TabIndex = 0
         '
         'GroupBox1
         '
@@ -51,15 +52,42 @@ Partial Class formProcesadorTexto
         '
         'GroupBox2
         '
-        Me.GroupBox2.Controls.Add(Me.Button3)
-        Me.GroupBox2.Controls.Add(Me.Button2)
-        Me.GroupBox2.Controls.Add(Me.Button1)
+        Me.GroupBox2.Controls.Add(Me.btnVerde)
+        Me.GroupBox2.Controls.Add(Me.btnAzul)
+        Me.GroupBox2.Controls.Add(Me.btnRojo)
         Me.GroupBox2.Location = New System.Drawing.Point(329, 126)
         Me.GroupBox2.Name = "GroupBox2"
         Me.GroupBox2.Size = New System.Drawing.Size(120, 164)
         Me.GroupBox2.TabIndex = 2
         Me.GroupBox2.TabStop = False
         Me.GroupBox2.Text = "Color de fuente"
+        '
+        'btnVerde
+        '
+        Me.btnVerde.Location = New System.Drawing.Point(7, 119)
+        Me.btnVerde.Name = "btnVerde"
+        Me.btnVerde.Size = New System.Drawing.Size(75, 23)
+        Me.btnVerde.TabIndex = 2
+        Me.btnVerde.Text = "Verde"
+        Me.btnVerde.UseVisualStyleBackColor = True
+        '
+        'btnAzul
+        '
+        Me.btnAzul.Location = New System.Drawing.Point(7, 76)
+        Me.btnAzul.Name = "btnAzul"
+        Me.btnAzul.Size = New System.Drawing.Size(75, 23)
+        Me.btnAzul.TabIndex = 1
+        Me.btnAzul.Text = "Azul"
+        Me.btnAzul.UseVisualStyleBackColor = True
+        '
+        'btnRojo
+        '
+        Me.btnRojo.Location = New System.Drawing.Point(7, 37)
+        Me.btnRojo.Name = "btnRojo"
+        Me.btnRojo.Size = New System.Drawing.Size(75, 23)
+        Me.btnRojo.TabIndex = 0
+        Me.btnRojo.Text = "Rojo"
+        Me.btnRojo.UseVisualStyleBackColor = True
         '
         'GroupBox3
         '
@@ -70,52 +98,25 @@ Partial Class formProcesadorTexto
         Me.GroupBox3.TabStop = False
         Me.GroupBox3.Text = "Opciones de Fuente"
         '
-        'Button1
+        'btnLimpiar
         '
-        Me.Button1.Location = New System.Drawing.Point(7, 37)
-        Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(75, 23)
-        Me.Button1.TabIndex = 0
-        Me.Button1.Text = "Rojo"
-        Me.Button1.UseVisualStyleBackColor = True
-        '
-        'Button2
-        '
-        Me.Button2.Location = New System.Drawing.Point(7, 76)
-        Me.Button2.Name = "Button2"
-        Me.Button2.Size = New System.Drawing.Size(75, 23)
-        Me.Button2.TabIndex = 1
-        Me.Button2.Text = "Azul"
-        Me.Button2.UseVisualStyleBackColor = True
-        '
-        'Button3
-        '
-        Me.Button3.Location = New System.Drawing.Point(7, 119)
-        Me.Button3.Name = "Button3"
-        Me.Button3.Size = New System.Drawing.Size(75, 23)
-        Me.Button3.TabIndex = 2
-        Me.Button3.Text = "Verde"
-        Me.Button3.UseVisualStyleBackColor = True
-        '
-        'Button4
-        '
-        Me.Button4.Location = New System.Drawing.Point(336, 342)
-        Me.Button4.Name = "Button4"
-        Me.Button4.Size = New System.Drawing.Size(75, 23)
-        Me.Button4.TabIndex = 4
-        Me.Button4.Text = "Limpiar"
-        Me.Button4.UseVisualStyleBackColor = True
+        Me.btnLimpiar.Location = New System.Drawing.Point(336, 342)
+        Me.btnLimpiar.Name = "btnLimpiar"
+        Me.btnLimpiar.Size = New System.Drawing.Size(75, 23)
+        Me.btnLimpiar.TabIndex = 4
+        Me.btnLimpiar.Text = "Limpiar"
+        Me.btnLimpiar.UseVisualStyleBackColor = True
         '
         'formProcesadorTexto
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
-        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.btnLimpiar)
         Me.Controls.Add(Me.GroupBox3)
         Me.Controls.Add(Me.GroupBox2)
         Me.Controls.Add(Me.GroupBox1)
-        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.txtResultado)
         Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
         Me.Name = "formProcesadorTexto"
         Me.Text = "ProcesadorTexto"
@@ -125,12 +126,12 @@ Partial Class formProcesadorTexto
 
     End Sub
 
-    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents txtResultado As TextBox
     Friend WithEvents GroupBox1 As GroupBox
     Friend WithEvents GroupBox2 As GroupBox
-    Friend WithEvents Button3 As Button
-    Friend WithEvents Button2 As Button
-    Friend WithEvents Button1 As Button
+    Friend WithEvents btnVerde As Button
+    Friend WithEvents btnAzul As Button
+    Friend WithEvents btnRojo As Button
     Friend WithEvents GroupBox3 As GroupBox
-    Friend WithEvents Button4 As Button
+    Friend WithEvents btnLimpiar As Button
 End Class
--- ProcesadorTexto.vb
+++ ProcesadorTexto.vb
@@ -1,3 +1,18 @@
 ﻿Public Class formProcesadorTexto
+    Private Sub btnRojo_Click(sender As Object, e As EventArgs) Handles btnRojo.Click
+        txtResultado.ForeColor = Color.Red
+    End Sub
 
+    Private Sub btnAzul_Click(sender As Object, e As EventArgs) Handles btnAzul.Click
+        txtResultado.ForeColor = Color.Blue
+    End Sub
+
+    Private Sub btnVerde_Click(sender As Object, e As EventArgs) Handles btnVerde.Click
+        txtResultado.ForeColor = Color.Green
+    End Sub
+
+    Private Sub btnLimpiar_Click(sender As Object, e As EventArgs) Handles btnLimpiar.Click
+        txtResultado.Text = ""
+        txtResultado.ForeColor = Color.Black
+    End Sub
 End Class
